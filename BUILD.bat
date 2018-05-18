f8bfa8ccc96276d5575a060bfae4ef6a8f069737
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2018-05-18 05:11:46 +0100

    Added avatar Changed footer logo to lemon Disabled toc Updated FontAwesome to 5.013

--- BUILD.bat
+++ BUILD.bat
@@ -1,19 +1,5 @@
 REM Removing old build
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\about"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\blog"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\categories"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\css"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\fonts"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\images"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\js"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\notes"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\page"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\projects"
-rmdir /S /Q "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\tags"
-del "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\404.html"
-del "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\index.html"
-del "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\index.xml"
-del "C:\Users\Lemon\Documents\Github\Web\tehlemon.com\public\sitemap.xml"
+rmdir /S /Q public
 
 REM Building site
 hugo
