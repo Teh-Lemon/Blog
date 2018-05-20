a633267700b5b8047205f9aca29b6f766248ac91
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2018-05-20 07:02:49 +0100

    Started HK 2018 Day 2 Blog Removed more unused theme fonts from build Blog summaries now use frontmatter description

--- BUILD.bat
+++ BUILD.bat
@@ -8,3 +8,5 @@ REM Removing theme files that aren't used
 rmdir /S /Q public\fonts
 del public\images\avatar.png
 del public\images\avatar@2x.png
+del public\css\monosocialiconsfont.css
+del public\css\font-awesome.min.css
--- archetypes/default.md
+++ archetypes/default.md
@@ -5,6 +5,5 @@ date = {{ .Date }}
 tags = [""]
 categories = [""]
 draft = false
+description = ""
 +++
-<!--more-->
-Boop.
