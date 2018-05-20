22a4e15cf6e7d802fb75c175f98f7ad97b72e6e7
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2018-05-20 04:23:28 +0100

    Fixed missing brand fonts Updated Hugo build steps Fixed some typos

--- BUILD.bat
+++ BUILD.bat
@@ -1,2 +1,10 @@
+REM Removing old build
 rmdir /S /Q public
+
+REM Building site
 hugo -b https://www.tehlemon.com/
+
+REM Removing theme files that aren't used
+rmdir /S /Q public\fonts
+del public\images\avatar.png
+del public\images\avatar@2x.png
