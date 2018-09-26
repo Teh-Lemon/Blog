5adcdcd957f663e7375e193bc9dc5a83ff767ffe
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2018-09-27 00:06:18 +0100

    Added image rotation to cmd cheat sheet

--- BUILD.bat
+++ BUILD.bat
@@ -6,8 +6,8 @@ hugo -b https://www.tehlemon.com/
 
 REM Removing theme files that aren't used
 rmdir /S /Q public\fonts
-del public\images\avatar.png
 del public\images\avatar@2x.png
+del public\images\avatar.png
 del public\css\monosocialiconsfont.css
 del public\css\font-awesome.min.css
 
