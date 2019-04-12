1a696e19a5bc518f3f0dfef34db115d2f1edfe2d
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2019-04-12 03:47:59 +0100

    Updated build script for mediumish Fixed build directory back to public

--- BUILD.bat
+++ BUILD.bat
@@ -5,11 +5,7 @@ REM Building site
 hugo -b https://www.tehlemon.com/
 
 REM Removing theme files that aren't used
-rmdir /S /Q public\fonts
-del public\images\avatar@2x.png
-del public\images\avatar.png
-del public\css\monosocialiconsfont.css
-del public\css\font-awesome.min.css
+REM None needed for Mediumish Theme
 
 REM Re-building git submodule
 echo gitdir: ../.git/modules/public> public/.git
--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# Personal website
-
-<https://www.tehlemon.com/>
