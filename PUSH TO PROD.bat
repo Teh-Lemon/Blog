a367fbe6a8770ec663f897719678033cd838d944
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2022-11-18 08:30:46 +0000

    Push to prod script now builds first

--- PUSH TO PROD.bat
+++ PUSH TO PROD.bat
@@ -1,6 +1,10 @@
+set /p commitmsg="Enter a commit message: "
+
+REM Building...
+call BUILD.bat
+
 REM Pushing changes to Github...
 cd public
-set /p commitmsg="Enter a commit message: "
 git add --all .
 git commit -m "%commitmsg%"
 pause & :: This pause is needed sometimes
